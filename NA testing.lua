7bfbb517bc41d37e4436f28ecf91237056864cc7
Aervanix <petkovmax320@gmail.com>
2025-05-22 00:33:11 +0300

    CHERRY UNC IDK

--- NA testing.lua
+++ NA testing.lua
@@ -5802,6 +5802,10 @@ cmd.add({"sUNCtest","sUNC"},{"sUNCtest (sUNC)","uses sUNC test that test the fun
 	loadstring(game:HttpGet("https://script.sunc.su/"))()
 end)
 
+cmd.add({"cherryUNC","CUNC"},{"cherryUNC (CUNC)","executes cherry's advanced UNC checker that actually checks your executor's UNC (if it crashes that's roblox issue)"},function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Executor-Tests/refs/heads/main/Environment/Test.lua", true))()
+end)
+
 cmd.add({"vulnerabilitytest","vulntest"},{"vulnerabilitytest (vulntest)","Test if your executor is Vulnerable"},function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/uuuuuuu/main/VulnTest.lua"))()
 end)
