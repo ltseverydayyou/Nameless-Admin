470832c29cb7e7dd0cf2f62341b28b1bdaa53ebb
Aervanix <117316014+ltseverydayyou@users.noreply.github.com>
2025-11-14 20:01:15 +0200

    3101266219

--- Source.lua
+++ Source.lua
@@ -1700,7 +1700,7 @@ NAmanage.createLoadingUI=function(text, opts)
 	}
 	opts = opts or {}
 	local widthScale = tonumber(opts.widthScale) or 0.34
-	local blacklist = opts.blacklist or { [8523781134] = true, [3101266219] = true, [2521585756] = true }
+	local blacklist = opts.blacklist or { [8523781134] = true, [2521585756] = true }
 	local lp = services.Players and services.Players.LocalPlayer
 
 	if lp and blacklist[lp.UserId] then
