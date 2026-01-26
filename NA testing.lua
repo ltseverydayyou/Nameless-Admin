311e2a8ebbc9a40b35b7de8e971a1a1646ab6b6b
ltseverydayyou <117316014+ltseverydayyou@users.noreply.github.com>
2026-01-26 21:42:04 +0200

    Update NA testing.lua

--- NA testing.lua
+++ NA testing.lua
@@ -50485,8 +50485,8 @@ NAgui.loadCMDS = function()
 				if predictionInput then
 					predictionInput.Text = ""
 				end
-				NAUIMANAGER.cmdInput:ReleaseFocus()
-				task.delay(0.03, function()
+				NAUIMANAGER.cmdInput:CaptureFocus()
+				task.delay(0.04, function()
 					if NAUIMANAGER.cmdInput then
 						NAUIMANAGER.cmdInput:CaptureFocus()
 						task.defer(function()
